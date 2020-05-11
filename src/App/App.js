3ff2366ea99954c828101d7ae531a7614980ef42
Zac Crumpton <z.crumpton@gmail.com>
2020-05-11 17:07:52 -0500

    updated app

--- src/App/App.js
+++ src/App/App.js
@@ -4,20 +4,9 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h2>Sports Roster</h2>
+    <button className="btn btn-dark"><i className="fas fa-rocket"></i></button>
+  </div>
   );
 }
 
