f785fa744831ae493018831987ca23a7194321ea
Zac Crumpton <z.crumpton@gmail.com>
2020-05-18 21:27:57 -0500

    base components seup

---
+++ src/components/Team/Team.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Team.scss';
+
+class Team extends React.Component {
+  render() {
+    return (
+      <div className='Team'>
+        <h1>TEAM COMP</h1>
+      </div>
+    );
+  }
+}
+
+export default Team;
