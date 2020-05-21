d610e89b68a462b893e5f6a0d5c5a37af98f05c8
Zac Crumpton <z.crumpton@gmail.com>
2020-05-20 19:43:03 -0500

    auth works

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,55 @@
 import React from 'react';
-import './App.scss';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from '../helpers/data/connection';
 
 import Auth from '../components/Auth/Auth';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
 import Team from '../components/Team/Team';
 
+import './App.scss';
+
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed) {
+        componentToLoad = <Team />;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
+
     return (
       <div className="App">
-      <h2>Sports Roster</h2>
-      <MyNavbar/>
-      <Auth/>
-      <Team/>
-    </div>
+        <MyNavbar authed={authed}/>
+        <h1>React Pinterest</h1>
+        {loadComponent()}
+      </div>
     );
   }
 }
